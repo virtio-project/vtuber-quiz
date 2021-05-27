93e5632c2d3760166ec8701029bc977c38afda25
lightsing <light.tsing@gmail.com>
2021-05-27 17:57:52 +0800

    set session when create success

--- backend/src/services.rs
+++ backend/src/services.rs
@@ -1,14 +1,22 @@
 use actix_web::{post, web};
-use actix_web::{Result, HttpResponse};
+use actix_web::{HttpResponse, Result};
+use actix_session::Session;
 
-use crate::Pool;
+use crate::error::Error;
 use crate::hcaptcha::Hcaptcha;
 use crate::models::{RegisterRequest, User};
-use crate::error::Error;
+use crate::Pool;
+
 
 #[post("/user")]
-pub async fn register(req: web::Json<RegisterRequest>, pool: web::Data<Pool>, _hcaptcha: Hcaptcha) -> Result<HttpResponse> {
+pub async fn register(
+    req: web::Json<RegisterRequest>,
+    pool: web::Data<Pool>,
+    session: Session,
+    _hcaptcha: Hcaptcha,
+) -> Result<HttpResponse> {
     let id = User::create(&pool, req.username.as_str(), req.password.as_str()).await?;
+    session.insert("user", id)?;
     Ok(HttpResponse::Ok().json(User::get_by_id(&pool, id).await?))
 }
 
