ecdc64997258d90e714082d23f10ef835186d743
lightsing <light.tsing@gmail.com>
2021-05-28 16:40:35 +0800

    add get post info

--- backend/src/bilibili.rs
+++ backend/src/bilibili.rs
@@ -8,7 +8,6 @@ use reqwest::get;
 struct Response<T> {
     code: i32,
     message: String,
-    ttl: i32,
     data: Option<T>
 }
 
@@ -17,7 +16,9 @@ enum Error {
     #[error(transparent)]
     Bilbili(#[from] BilbiliError),
     #[error(transparent)]
-    Reqwest(#[from] reqwest::Error)
+    Reqwest(#[from] reqwest::Error),
+    #[error("got unexpected response")]
+    UnexpectedResponse,
 }
 
 #[derive(Clone, Debug, Serialize, thiserror::Error)]
@@ -36,6 +37,13 @@ pub struct AccountInfo {
     pub level: u8,
     #[serde(deserialize_with = "from_u8")]
     pub silence: bool,
+}
+
+#[derive(Clone, Debug, Serialize)]
+pub struct PostInfo {
+    pub rid: u64,
+    pub sender: AccountInfo,
+    pub content: String,
 }
 
 impl <T> Response<T> {
@@ -57,6 +65,46 @@ impl AccountInfo {
         } else {
             Err(response.into_error())?
         }
+    }
+}
+
+impl PostInfo {
+    async fn get_by_id(rid: u64) -> Result<Self, Error> {
+        let url = format!("https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_svr/get_dynamic_detail?dynamic_id={}", rid);
+        let response: Response<serde_json::Value> = get(url).await?.json().await?;
+        if !response.success() {
+            return Err(response.into_error().into());
+        }
+        let data = response.data.unwrap();
+        let inner = data
+            .get("card")
+            .ok_or(Error::UnexpectedResponse)?;
+        let card = inner
+            .get("card")
+            .and_then(|v| v.as_str())
+            .and_then(|s| serde_json::from_str::<serde_json::Value>(s).ok())
+            .ok_or(Error::UnexpectedResponse)?;
+        let uid = card
+            .get("user")
+            .and_then(|u| u.get("uid"))
+            .and_then(|u| u.as_u64())
+            .ok_or(Error::UnexpectedResponse)?;
+        let sender = AccountInfo::get_by_id(uid).await?;
+        let item = card.get("item").ok_or(Error::UnexpectedResponse)?;
+        let rid = item
+            .get("rp_id")
+            .and_then(|u| u.as_u64())
+            .ok_or(Error::UnexpectedResponse)?;
+        let content = item
+            .get("content")
+            .and_then(|v| v.as_str())
+            .map(|s| s.to_string())
+            .ok_or(Error::UnexpectedResponse)?;
+        Ok(Self {
+            sender,
+            rid,
+            content
+        })
     }
 }
 
@@ -113,5 +161,14 @@ mod tests {
     async fn test_get_account_info_err() {
         let err = AccountInfo::get_by_id(1000000000).await.unwrap_err().unwrap_bilibili();
         assert_eq!(err.code, -404);
+    }
+
+    #[tokio::test]
+    async fn test_get_post_info() {
+        let info = PostInfo::get_by_id(85387458835565545).await.unwrap();
+        assert_eq!(info.rid, 85387458833966060);
+        assert_eq!(info.sender.uid, 2);
+        assert_eq!(info.sender.name, "碧诗");
+        assert!(!info.content.is_empty());
     }
 }
