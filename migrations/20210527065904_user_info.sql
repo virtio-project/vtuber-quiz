2e2c0d3ea2c4b14ba977cc3be646e2d79fbc6266
lightsing <light.tsing@gmail.com>
2021-05-29 13:49:45 +0800

    add comments and refactor

--- migrations/20210527065904_user_info.sql
+++ migrations/20210527065904_user_info.sql
@@ -1,31 +1,48 @@
+-- 用户类型：普通、vtuber
+create type user_role as enum ('normal', 'vtuber');
+
 create table "user"
 (
     id         serial primary key,
+    -- 用户名，唯一
     username   varchar(20) not null unique,
+    -- 密码，argon2存储
     password   text        not null,
+    -- 绑定b站用户时的挑战字符串，未创建时为空
     challenge  varchar(10),
+    -- 是否封锁
     blocked    boolean     not null default false,
+    -- 用户类型
     role       user_role   not null default 'normal'::user_role,
+    -- 用户声望，初始为0
     reputation integer     not null default 0,
     created    timestamptz not null default current_timestamp,
     updated    timestamptz not null default current_timestamp
 );
 
+-- 挑战字符串对用户唯一（创建后）
 create unique index on "user"(challenge) where challenge is not null;
 
 create table bilibili
 (
+    -- b站用户uid，唯一
     uid     integer primary key check ( uid > 0 ),
+    -- b站昵称
     name    varchar(20) unique        not null,
+    -- 用户头像
     avatar  text                      not null,
+    -- 绑定的用户
     "user"  integer references "user" not null unique,
     created timestamptz               not null default current_timestamp
 );
 
 create table following
 (
+    -- 用户
     follower integer references "user" not null,
+    -- 被关注的用户
     followee integer references "user" not null check ( follower <> followee ),
+    -- 悄悄关注
     private  boolean                   not null default false,
     created  timestamptz               not null default current_timestamp,
     unique (follower, followee)
--- migrations/20210527065929_question_info.sql
+++ migrations/20210527065929_question_info.sql
@@ -1,12 +1,28 @@
+-- 题目类型：判断题、单选、多选
+create type question_type as enum ('true_false', 'multi_choice', 'multi_answer');
+-- 投票类型：顶、踩、标记过时、标记错误
+create type vote_action as enum ('up_vote', 'down_vote', 'flag_outdated', 'flag_incorrect');
+-- 题目适用的对象：vtuber自己、粉丝、路人（？有必要吗）
+create type audience as enum ('vtuber', 'fan', 'passenger');
+
 create table question
 (
     id          serial primary key,
+    -- 问题创建者
     creator     integer references "user" not null,
+    -- 问题描述
     description text                      not null,
+    -- 选项，判断题恒为['Y', 'N']
     choices     text[]                    not null,
+    -- 答案，判断题为[0]或[1]，单选题为单个选项数字，多选题为所有正确的选项的数字
     answer      integer[]                 not null,
+    -- 题目类型
     type        question_type             not null,
+    -- 题目适用的对象
+    audiences   audience[]                not null,
+    -- 是否为草稿（仅自己可见）
     draft       boolean                   not null,
+    -- 是否删除
     deleted     boolean                   not null default false,
     created     timestamptz               not null default current_timestamp,
     updated     timestamptz               not null default current_timestamp
@@ -17,8 +33,11 @@ create index on question (type);
 create table vote
 (
     id       serial primary key,
+    -- 投票人
     voter    integer references "user"   not null,
+    -- 被投票的问题
     question integer references question not null,
+    -- 投票的类型
     action   vote_action                 not null,
     created  timestamptz                 not null default current_timestamp,
     unique (question, voter)
@@ -26,7 +45,9 @@ create table vote
 
 create table apply_to
 (
+    -- 问题
     question integer references question not null,
+    -- 问题归属的 vtuber （一个问题可以有多个归属）
     vtuber   integer references "user"   not null,
     created  timestamptz                 not null default current_timestamp,
     unique (vtuber, question)
