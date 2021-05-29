7b4af7967fed6a27678255aaee82654ecf33b9ca
lightsing <light.tsing@gmail.com>
2021-05-29 20:16:36 +0800

    add QuestionCreationRequest

--- commons/src/models.rs
+++ commons/src/models.rs
@@ -25,6 +25,34 @@ pub struct ChallengeResponse {
     pub templates: Vec<String>
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum QuestionCreationRequest {
+    TrueFalse(TrueFalseQuestion),
+    MultiChoice(MultiChoiceQuestion),
+    MultiAnswer(MultiAnswerQuestion),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TrueFalseQuestion {
+    description: String,
+    is_true: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct MultiChoiceQuestion {
+    description: String,
+    choices: Vec<String>,
+    answer: usize,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct MultiAnswerQuestion {
+    description: String,
+    choices: Vec<String>,
+    answer: Vec<usize>,
+}
+
 #[cfg_attr(feature = "backend", derive(sqlx::Type))]
 #[cfg_attr(feature = "backend", sqlx(type_name = "user_role", rename_all = "lowercase"))]
 #[derive(Copy, Clone, Debug, Serialize, Deserialize, Eq, PartialEq)]
@@ -46,7 +74,7 @@ pub enum QuestionType {
 
 #[cfg_attr(feature = "backend", derive(sqlx::Type))]
 #[cfg_attr(feature = "backend", sqlx(type_name = "vote_action", rename_all = "snake_case"))]
-#[derive(Copy, Clone, Debug, Serialize, Eq, PartialEq)]
+#[derive(Copy, Clone, Debug, Serialize, Deserialize, Eq, PartialEq)]
 #[serde(rename_all = "snake_case")]
 pub enum VoteAction {
     UpVote,
@@ -57,7 +85,7 @@ pub enum VoteAction {
 
 #[cfg_attr(feature = "backend", derive(sqlx::Type))]
 #[cfg_attr(feature = "backend", sqlx(type_name = "audience", rename_all = "lowercase"))]
-#[derive(Copy, Clone, Debug, Serialize, Eq, PartialEq)]
+#[derive(Copy, Clone, Debug, Serialize, Deserialize, Eq, PartialEq)]
 #[serde(rename_all = "snake_case")]
 pub enum Audience {
     Vtuber,
@@ -118,4 +146,75 @@ impl ChallengeResponse {
     }
 }
 
+impl QuestionCreationRequest {
+    pub fn unwrap_true_false(self) -> TrueFalseQuestion {
+        if let Self::TrueFalse(q) = self {
+            q
+        } else {
+            panic!("variant mismatch")
+        }
+    }
+
+    pub fn unwrap_multi_choice(self) -> MultiChoiceQuestion {
+        if let Self::MultiChoice(q) = self {
+            q
+        } else {
+            panic!("variant mismatch")
+        }
+    }
+
+    pub fn unwrap_multi_answer(self) -> MultiAnswerQuestion {
+        if let Self::MultiAnswer(q) = self {
+            q
+        } else {
+            panic!("variant mismatch")
+        }
+    }
+}
+
 const fn default_false() -> bool { false }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_true_false() {
+        let tf_json: &str = r#"{
+  "description": "1+1=3",
+  "is_true": false
+}"#;
+        let question: QuestionCreationRequest = serde_json::from_str(tf_json).unwrap();
+        let true_false = question.unwrap_true_false();
+        assert_eq!(true_false.description.as_str(), "1+1=3");
+        assert_eq!(true_false.is_true, false);
+    }
+
+    #[test]
+    fn test_multi_choice() {
+        let mc_json: &str = r#"{
+  "description": "select a prime number from those numbers",
+  "choices": ["1", "2", "4", "8"],
+  "answer": 1
+}"#;
+        let question: QuestionCreationRequest = serde_json::from_str(mc_json).unwrap();
+        let multi_choice = question.unwrap_multi_choice();
+        assert_eq!(multi_choice.description.as_str(), "select a prime number from those numbers");
+        assert_eq!(multi_choice.choices.len(), 4);
+        assert_eq!(multi_choice.answer, 1);
+    }
+
+    #[test]
+    fn test_multi_answer() {
+        let ma_json: &str = r#"{
+  "description": "select all words describe color",
+  "choices": ["black", "red", "green", "happy"],
+  "answer": [0, 1, 2]
+}"#;
+        let question: QuestionCreationRequest = serde_json::from_str(ma_json).unwrap();
+        let multi_answer = question.unwrap_multi_answer();
+        assert_eq!(multi_answer.description.as_str(), "select all words describe color");
+        assert_eq!(multi_answer.choices.len(), 4);
+        assert_eq!(multi_answer.answer, vec![0, 1, 2]);
+    }
+}
