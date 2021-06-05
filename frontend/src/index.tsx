e0b7cdee7b8ed8fbe62a982bbaff6d736e11daf6
lightsing <light.tsing@gmail.com>
2021-06-06 02:21:50 +0800

    add baseweb

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider as StyletronProvider, DebugEngine } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const debug =
+  process.env.NODE_ENV === "production" ? void 0 : new DebugEngine();
+
+const engine = new Styletron();
+
 ReactDOM.render(
-  <React.StrictMode>
+  <StyletronProvider value={engine} debug={debug} debugAfterHydration>
     <App />
-  </React.StrictMode>,
+  </StyletronProvider>,
   document.getElementById('root')
 );
 
