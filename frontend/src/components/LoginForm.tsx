a9c80ac0291451a94cb860dcac2799a039282ab6
lightsing <light.tsing@gmail.com>
2021-06-06 21:01:35 +0800

    improve interaction

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { FormControl } from 'baseui/form-control';
 import { Input } from 'baseui/input';
 import { Button } from 'baseui/button';
-import { useStyletron } from 'baseui';
 import { Captcha } from './Captcha';
 import HCaptcha from '@hcaptcha/react-hcaptcha';
 
@@ -10,27 +9,36 @@ const LoginForm: React.FC = () => {
   const [username, setUsername] = React.useState<string | null>(null);
   const [password, setPassword] = React.useState<string | null>(null);
   const [token, setToken] = React.useState<string | null>(null);
+  const [usernameErr, setUsernameErr] = React.useState(false);
+  const [passwordErr, setPasswordErr] = React.useState(false);
   const captchaRef = React.useRef<HCaptcha>(null);
-  const [css] = useStyletron();
 
   const onSubmit = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
-    if (token === null) {
+    const invalidUsername = username === null || username.trim() === '';
+    const invalidPassword = password === null || password.trim() === '';
+    const formReady = !invalidUsername && !invalidPassword;
+
+    setUsernameErr(invalidUsername);
+    setPasswordErr(invalidPassword);
+    if (formReady && token === null) {
       captchaRef.current?.execute()
     }
+
     console.log(`username: ${username}; password: ${password}; token: ${token}`)
   };
 
   return (
-    <form className={css({
-      maxWidth: '300px',
-      padding: '0 25px'
-    })}>
+    <form>
       <FormControl>
         <Input
           placeholder="用户名"
           value={username === null ? '' : username}
-          onChange={event => setUsername(event.currentTarget.value)}
+          onChange={event => {
+            setUsernameErr(false);
+            setUsername(event.currentTarget.value);
+          }}
+          error={usernameErr}
           required
         />
       </FormControl>
@@ -39,7 +47,11 @@ const LoginForm: React.FC = () => {
           placeholder="密码"
           type="password"
           value={password === null ? '' : password}
-          onChange={event => setPassword(event.currentTarget.value)}
+          onChange={event => {
+            setPasswordErr(false);
+            setPassword(event.currentTarget.value);
+          }}
+          error={passwordErr}
           required
         />
       </FormControl>
