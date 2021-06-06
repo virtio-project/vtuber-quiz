20e486810bc29d7c8c26a3a41537b3f481d937fb
lightsing <light.tsing@gmail.com>
2021-06-06 15:54:54 +0800

    override HeaderNavigation style

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -7,34 +7,35 @@ import {
 } from 'baseui/header-navigation';
 import {StyledLink as Link} from 'baseui/link';
 import {Button} from 'baseui/button';
-import { useStyletron } from 'baseui';
 
 const Nav: React.FC = () => {
-  const [css] = useStyletron();
   return (
-    <header className={css({
-      padding: '0 25px'
-    })}>
-      <HeaderNavigation>
-        <NavigationList $align={ALIGN.left}>
-          <NavigationItem>Vtuber 测试平台</NavigationItem>
-        </NavigationList>
-        <NavigationList $align={ALIGN.center} />
-        <NavigationList $align={ALIGN.right}>
-          <NavigationItem>
-            <Link href="#">Tab Link One</Link>
-          </NavigationItem>
-          <NavigationItem>
-            <Link href="#">Tab Link Two</Link>
-          </NavigationItem>
-        </NavigationList>
-        <NavigationList $align={ALIGN.right}>
-          <NavigationItem>
-            <Button>Get started</Button>
-          </NavigationItem>
-        </NavigationList>
-      </HeaderNavigation>
-    </header>
+    <HeaderNavigation overrides={{
+      Root: {
+        style: ({ _ }) => ({
+          paddingLeft: '25px',
+          paddingRight: '30px',
+        })
+      }
+    }}>
+      <NavigationList $align={ALIGN.left}>
+        <NavigationItem>Vtuber 测试平台</NavigationItem>
+      </NavigationList>
+      <NavigationList $align={ALIGN.center} />
+      <NavigationList $align={ALIGN.right}>
+        <NavigationItem>
+          <Link href="#">Tab Link One</Link>
+        </NavigationItem>
+        <NavigationItem>
+          <Link href="#">Tab Link Two</Link>
+        </NavigationItem>
+      </NavigationList>
+      <NavigationList $align={ALIGN.right}>
+        <NavigationItem>
+          <Button>Get started</Button>
+        </NavigationItem>
+      </NavigationList>
+    </HeaderNavigation>
   );
 };
 
